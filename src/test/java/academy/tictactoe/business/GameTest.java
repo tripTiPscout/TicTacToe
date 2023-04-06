1dc628cb29b693dcf704efe8bb29cf08a4710faf
Peter Stoilkov <peter_sto@mail.bg>
2023-04-06 16:00:02 +0300

    TicTacToe -New game test added

--- src/test/java/academy/tictactoe/business/GameTest.java
+++ src/test/java/academy/tictactoe/business/GameTest.java
@@ -1,12 +1,10 @@
 package academy.tictactoe.business;
 
-
 import academy.tictactoe.data.GameSetUp;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertSame;
-
 
 public class GameTest {
 
